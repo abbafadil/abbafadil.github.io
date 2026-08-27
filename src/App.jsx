cebb6f215531ba372d4d5e6f909af83ba8b6e656
abbafadil <aldymaldwn@gmail.com>
2026-08-27 19:56:09 +0300

    Map with array lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,16 @@
 import { useState } from "react";
 
 function App(){
-  const user = [
-    {
-      name : "Abbas" ,
-      age : 22 ,
-      major : "Front-end"
-    },
-
-    {
-      name : "Ahmed" ,
-      age : 26 ,
-      major : "IT"
-    },
-
-    {
-      name : "Saramad" ,
-      age : 22,
-      major : "Back-end"
-    }
-  ];
-
-      return(
-         
-        <>
-       
-        {user.map((user) =>{
-          
-            return (
-<div>
-<h1>{user.name}</h1>
-            <p>{user.age}</p>
-            <p>{user.major}</p>
-            </div>
-
-            ); 
-        })}
-        </>
+  const favg = 
+    ["Minecraft", "Fifa27", "RocketLouge"];
+    return(
+      <>
+{favg.map((game) =>{
+  return <h2 key={game}>{game}</h2>;
         
-      );
+      })};
+      </>
+    );
     
 }
 
