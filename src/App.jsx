791ee642187982faf1d47af9a4dd918ae9c6e5e8
abbafadil <aldymaldwn@gmail.com>
2026-09-05 14:14:22 +0300

    key with map lol

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App(){
       name : "FIFA",
       company : "EA" ,
       year : 2023
-    }];
+    }]
     return(
       <>
 {favg.map((game) =>{
@@ -37,4 +37,4 @@ function App(){
 }
 
 
-export default App; 
+export default App'
