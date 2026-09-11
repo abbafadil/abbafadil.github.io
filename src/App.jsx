4ec25cef6c75ad36720196f7f3cfe5456a59c3df
abbafadil <aldymaldwn@gmail.com>
2026-09-11 04:24:51 +0300

    Regaldress I'm sick today, but I contenno the progres

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,44 @@
 import { useState } from "react";
 
-function App(){
-  const [favg, setFavg] = useState([{
-      name : "Minecraft" ,
-      company : "Mojang" ,
-      year : 2011
-    },
-    {
-      name : "GTA V" ,
-      company : "Rockstar",
-      year : 2013
-    },])
-    
-    return(
-      <>
-      
-
-{favg.map((game) => {
+function Game({ name, company, year }) {
   return (
-    <div key={game.name}>
-      <h1>{game.name}</h1>
-      <h2>{game.company}</h2>
-      <h2>{game.year}</h2>
-
-      <button onClick={() => {
-        setFavg(
-          favg.map((game) => {
-           if (game.name === "GTA V") {
-            return{
-              ...game,
-              year : 2025
-            };
-           }
-           return game;
-          })
-        );
-      }}>
-        update GTA V
-      </button>
+    <div>
+      <h1>{name}</h1>
+      <h2>{company}</h2>
+      <h2>{year}</h2>
     </div>
   );
-})}
-  
-  
-      </>
-    );
-    
 }
 
+function App() {
+  const [favg, setFavg] = useState([
+    {
+      name: "Minecraft",
+      company: "Mojang",
+      year: 2011
+    },
+    {
+      name: "GTA V",
+      company: "Rockstar",
+      year: 2013
+    }
+  ]);
+
+  return (
+    <>
+      <Game
+        name="Minecraft"
+        company="Mojang"
+        year={2011}
+      />
+
+      <Game
+        name="GTA V"
+        company="Rockstar"
+        year={2013}
+      />
+    </>
+  );
+}
 
 export default App;
