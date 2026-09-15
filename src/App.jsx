0c682c3eaea0dd4090f551798491bf2dfd2c5cdb
abbafadil <aldymaldwn@gmail.com>
2026-09-15 06:23:13 +0300

    add another game, lol

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,9 @@ function App() {
       year: 2011
     },
     {
-      name: "FIFA 25",
-      company: "EA",
-      year: 2025
+      name: "GTA V",
+      company: "Rockstar",
+      year: 2013
     }
   ]);
 
